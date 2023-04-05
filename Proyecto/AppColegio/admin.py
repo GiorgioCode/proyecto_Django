1b28946bd727ebdb9063c00cbe57d8b88e228d2b
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2023-04-05 09:34:39 -0300

    modificacion modelos y claves

--- Proyecto/AppColegio/admin.py
+++ Proyecto/AppColegio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Curso, Entregable, Profesor, Estudiante
+
+admin.site.register(Curso)
+admin.site.register(Estudiante)
+admin.site.register(Profesor)
+admin.site.register(Entregable)
--- Proyecto/AppColegio/models.py
+++ Proyecto/AppColegio/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 
 
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+
+
 class Curso(models.Model):
     nombre = models.CharField(max_length=50)
     comision = models.IntegerField()
     codigo = models.CharField(max_length=10)
 
-
-class Estudiante(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254)
+    def __str__(self):
+        return f'{self.nombre} - {self.comision}'
+    estudiante = models.ForeignKey(
+        Estudiante, on_delete=models.CASCADE, null="Desconocido")
 
 
 class Profesor(models.Model):
@@ -19,8 +27,16 @@ class Profesor(models.Model):
     email = models.EmailField(max_length=254)
     profesion = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+
 
 class Entregable(models.Model):
     nombre = models.CharField(max_length=50)
     fecha_entrega = models.DateField()
     entregado = models.BooleanField()
+    estudiante = models.ForeignKey(
+        Estudiante, on_delete=models.CASCADE, null="Desconocido")
+
+    def __str__(self):
+        return f'{self.nombre} - {self.fecha_entrega}'
